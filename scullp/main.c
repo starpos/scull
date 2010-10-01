c889685b16a1c3d21669ba1358a4ae8a9a7b792c
HOSHINO Takashi <hoshino@labs.cybozu.co.jp>
2010-10-01 16:07:54 +0900

    modify to build scullp for 2.6.35.

--- scullp/main.c
+++ scullp/main.c
@@ -15,7 +15,6 @@
  * $Id: _main.c.in,v 1.21 2004/10/14 20:11:39 corbet Exp $
  */
 
-#include <linux/config.h>
 #include <linux/module.h>
 #include <linux/moduleparam.h>
 #include <linux/init.h>
@@ -400,32 +399,65 @@ loff_t scullp_llseek (struct file *filp, loff_t off, int whence)
 struct async_work {
 	struct kiocb *iocb;
 	int result;
-	struct work_struct work;
+	struct delayed_work work;
 };
 
 /*
  * "Complete" an asynchronous operation.
  */
-static void scullp_do_deferred_op(void *p)
+static void scullp_do_deferred_op(struct work_struct *work)
 {
-	struct async_work *stuff = (struct async_work *) p;
+        struct delayed_work *dwork = container_of(work, struct delayed_work, work);
+	struct async_work *stuff = container_of(dwork, struct async_work, work);
 	aio_complete(stuff->iocb, stuff->result, 0);
 	kfree(stuff);
 }
 
+/*
+ * Convert buffer interface
+ * from (char __user *buf, size_t count)
+ * to   (struct iovec *iov, unsigned long nr_seg)
+ */
+static int scullp_io_primitive(int write, struct kiocb *iocb,
+                               const struct iovec *iov,
+                               unsigned long nr_segs, loff_t pos)
+{
+        int result, tmp_result, i;
+        loff_t tmp_pos;
 
-static int scullp_defer_op(int write, struct kiocb *iocb, char __user *buf,
-		size_t count, loff_t pos)
+        result = 0;
+        tmp_result = 0;
+        for (i = 0; i < nr_segs; i ++) {
+                tmp_pos = pos + result;
+                if (write) {
+                        tmp_result = scullp_write
+                                (iocb->ki_filp, iov[i].iov_base,
+                                 iov[i].iov_len, &tmp_pos);
+                } else {
+                        tmp_result = scullp_read
+                                (iocb->ki_filp, iov[i].iov_base,
+                                 iov[i].iov_len, &tmp_pos);
+                }
+                if (tmp_result < 0) {
+                        result = tmp_result;
+                        break;
+                } else {
+                        result += tmp_result;
+                }
+        }
+        return result;
+}
+
+
+static int scullp_defer_op(int write, struct kiocb *iocb, const struct iovec *iov,
+                           unsigned long nr_segs, loff_t pos)
 {
 	struct async_work *stuff;
 	int result;
 
 	/* Copy now while we can access the buffer */
-	if (write)
-		result = scullp_write(iocb->ki_filp, buf, count, &pos);
-	else
-		result = scullp_read(iocb->ki_filp, buf, count, &pos);
-
+        result = scullp_io_primitive(write, iocb, iov, nr_segs, pos);
+        
 	/* If this is a synchronous IOCB, we return our status now. */
 	if (is_sync_kiocb(iocb))
 		return result;
@@ -436,24 +468,29 @@ static int scullp_defer_op(int write, struct kiocb *iocb, char __user *buf,
 		return result; /* No memory, just complete now */
 	stuff->iocb = iocb;
 	stuff->result = result;
-	INIT_WORK(&stuff->work, scullp_do_deferred_op, stuff);
+	INIT_DELAYED_WORK(&stuff->work, scullp_do_deferred_op);
 	schedule_delayed_work(&stuff->work, HZ/100);
 	return -EIOCBQUEUED;
 }
 
 
-static ssize_t scullp_aio_read(struct kiocb *iocb, char __user *buf, size_t count,
-		loff_t pos)
+/* static ssize_t scullp_aio_read
+   (struct kiocb *iocb, char __user *buf, size_t count, loff_t pos)
+*/
+static ssize_t scullp_aio_read(struct kiocb *iocb, const struct iovec *iov,
+                               unsigned long nr_segs, loff_t pos)
 {
-	return scullp_defer_op(0, iocb, buf, count, pos);
+	return scullp_defer_op(0, iocb, iov, nr_segs, pos);
 }
 
-static ssize_t scullp_aio_write(struct kiocb *iocb, const char __user *buf,
+/* static ssize_t scullp_aio_write(struct kiocb *iocb, const char __user *buf,
 		size_t count, loff_t pos)
+*/
+static ssize_t scullp_aio_write(struct kiocb *iocb, const struct iovec *iov,
+                                unsigned long nr_segs, loff_t pos)
 {
-	return scullp_defer_op(1, iocb, (char __user *) buf, count, pos);
+	return scullp_defer_op(1, iocb, iov, nr_segs, pos);
 }
-
 
  
 /*
